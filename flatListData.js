b3f94c0fcdcf749caaaf219e3d3e07d201d7cca6
Srinivasa Pulakanam <spulakanam@rccl.com>
2020-07-17 18:00:47 -0400

    updating data

--- flatListData.js
+++ flatListData.js
@@ -1,33 +1,33 @@
 var flatListData = [
     {
         "key": "598a678278fee204ee51cd2c",
-        "name": "John",   
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/b/bf/Cornish_cream_tea_2.jpg",                    
+        "name": "John Doe",   
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6c/Vegetable_Cart_in_Guntur.jpg",                    
         "foodDescription": "Looking for a box of groceries"
     },
     {
         "key": "598a684f78fee204ee51cd2f",
         "name": "Bob",        
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6e/Lactarius_indigo_48568.jpg",    
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/1/12/Lautrec_in_bed_1893.jpg",    
         "foodDescription": "Looking for blankets to keep my family warm"
     },
     {
         "key": "598a687678fee204ee51cd30",
         "name": "Victor",
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/d/d2/Oysters_served_on_ice%2C_with_lemon_and_parsley.jpg",
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/6/65/12-08-18-tilidin-retard.jpg",
         "foodDescription": "Need medication"
     },
     {
         "key": "598a680178fee204ee51cd2e",
         "name": "Ali",        
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/7/74/Yeolmukimchi_3.jpg",
-        "foodDescription": "Need food"
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Jeans.jpg",
+        "foodDescription": "Need clothes"
     },
     {
         "key": "598a688878fee204ee51cd31",
         "name": "Morgan",     
-        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/9/94/Salad_platter.jpg",   
-        "foodDescription": "Need help"
+        "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/3/3e/Dusche-cut-w025-h025.jpg",   
+        "foodDescription": "Need shower"
     },
     {
         "key": "598a68b778fee204ee51cd32",
