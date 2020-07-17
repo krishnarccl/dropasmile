8f7de8576bd859d6b4f4e6622277e232fdc92cbd
Srinivasa Pulakanam <spulakanam@rccl.com>
2020-07-17 14:59:02 -0400

    Login screen

--- App.js
+++ App.js
@@ -1,45 +1,23 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { Image, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import MapView, { Marker } from 'react-native-maps'
-import Emoji from 'react-native-emoji'
+
 export default class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-    };
-  }
-
-  ShowHideComponent = () => {
-    if (this.state.show == true) {
-      this.setState({ show: false });
-    } else {
-      this.setState({ show: true });
-    }
-  };
-
   render() {
     return (
-      <MapView
-        style={{ ...StyleSheet.absoluteFillObject }}
-        initialRegion={{
-          latitude: 25.7664362,
-          longitude: -80.1915964,
-          latitudeDelta: .005,
-          longitudeDelta: .005
-        }} onPress={this.ShowHideComponent} >
-        {this.state.show ?
-          <Marker
-            coordinate={{ latitude: 25.7664362, longitude: -80.1915964 }}
-            title='Brickell City Centre'
-            description='Help someone in need'
-          >
-            <Emoji name="disappointed" style={{ fontSize: 35 }} />
-          </Marker >
-          : null}
-        <View style={styles.bottom}><Text style={styles.titleText}>Tap anywhere drop a smile</Text></View>
-      </MapView>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Login</Text>
+        <TextInput style={styles.input} placeholder="Username"></TextInput>
+        <TextInput style={styles.input} placeholder="Password" secureTextEntry></TextInput>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.userBtn} onPress={() => {alert("This a test")}}>
+            <Text style={styles.btnTxt}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.userBtn}>
+            <Text style={styles.btnTxt}>SignUp</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
@@ -47,24 +25,66 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1e90ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  welcome: {
+    fontSize: 30,
+    justifyContent: 'center',
+    margin: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+
+  logintext: {
+    fontSize: 30,
+    justifyContent: 'center',
+    margin: 10,
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontFamily: "sans-serif-condensed",
+    color: '#fff',
+  },
+
+  input: {
+    backgroundColor:'#fff',
+    marginBottom: 10,
+    width: '90%',
+    padding: 15, 
+  },
+
+  btntxt: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  btn: {
+    backgroundColor: '#00FF00',
+    padding: 10,
+    width: '45%',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+
+  userBtn: {
+    backgroundColor: '#FFD700',
+    padding: 15,
+    width: '45%',
+  },
+
+  btnTxt: {
+    fontSize: 18,
     textAlign: 'center',
-    color: 'gray'
+
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  overlay: {
-    position: 'absolute',
-    bottom: 50,
-  },
-  bottom: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    marginBottom: 36
+
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%'
   }
 });
